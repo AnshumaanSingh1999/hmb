533ce07191fc1eb565d285da2d313f506e1be879
Anshumaan Singh <anshumaanborntolead@gmail.com>
2020-07-26 15:11:40 +0530

    intern hoo bhai

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      age: null,
+    };
+  }
+  mySubmitHandler = (event) => {
+    event.preventDefault();
+    let age = this.state.phone;
+    if (!Number(age)) {
+      alert("Your age must be a number");
+    }
+    else{
+      alert("Thanks")
+    }
+  }
+  myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({[nam]: val});
+  }
+  render() {
+    return (
+      <div> 
+      <form onSubmit={this.mySubmitHandler}>
+      <h1>Sign Up</h1>
+      <p>Name</p>
+      <input
+        type='text'
+        name='username'
+        onChange={this.myChangeHandler}
+        
+      />
+      <p>EmailID</p>
+      <input
+        type='text'
+        name='emailid'
+        onChange={this.myChangeHandler}
+      />
+      <p>PhoneNumber</p>
+      <input
+        type='text'
+        name='phone'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        onChange={this.myChangeHandler}
+      />
+      <p>Password</p>
+      
+      <input
+        type='text'
+        name='password'
+        onChange={this.myChangeHandler}
+      />
+      <p>Gender</p>
+      <select>
+  <option selected value="grapefruit">Male</option>
+  <option value="lime">Female</option>
+  <option value="coconut">Others</option>
+</select>
+      <br/>
+      <br/>
+      
+      <button>Sign Up</button>
+      </form>
+      <form>
+      <h5>Have an Account</h5>
+      <button>Log In</button>
+      </form>
+      </div>
+    );
+  }
 }
 
-export default App;
+ReactDOM.render(<MyForm />, document.getElementById('root'));
+
+export default MyForm
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Reservation from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Reservation />
   </React.StrictMode>,
   document.getElementById('root')
 );
